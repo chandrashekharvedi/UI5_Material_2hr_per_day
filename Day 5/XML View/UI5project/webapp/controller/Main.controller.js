f2164b5fe512bc4f4f56770458b49ddb7d3f5c84
kuldeepsingh2813 <75466950+kuldeepsingh2813@users.noreply.github.com>
2022-02-21 18:13:18 +0530

    Day 5

--- Day 5/XML View/UI5project/webapp/controller/Main.controller.js
+++ Day 5/XML View/UI5project/webapp/controller/Main.controller.js
@@ -8,7 +8,7 @@ sap.ui.define([
         onInit: function(){},
         onPress: function(){
             // I want to read the Value of the Input field and
-            //Step 2 : Call byId method of the application to get the control object by using this.getView()
+            //Step 2 : First get the view object using this.getView() and then by Using byId get the Ui5 element object
             var oInp = this.getView().byId("idInp");
             //Step 3 : On this object you can use the functions
             var oVal = oInp.getValue();
